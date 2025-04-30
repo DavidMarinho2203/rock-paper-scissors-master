1c6a0ff4f14d8777c55915bc0b1c7a57d9d0cacc
David Beckham <davidbeckhampaulo32@gmail.com>
2025-04-30 17:43:56 -0300

    fix: :bug: Atualizando o LocalStorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,38 +6,35 @@ import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-
-  const [score, setScore] = useState(0);
+  const [score, setScore] = useState(0); // Inicia como 0
   const [rules, setRules] = useState(false);
 
   useEffect(() => {
-
     const storedScore = localStorage.getItem("score");
-
     if (storedScore) {
       setScore(parseInt(storedScore));
-    }else{
-      localStorage.setItem("score", score.toString());
     }
-  }, [score])
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("score", score.toString());
+  }, [score]);
 
   return (
     <div className="relative bg-gradient-bg min-h-screen flex flex-col justify-between p-12 gap-10">
-
       <Header number={score} />
-
       <Game setScore={setScore} />
-
       <div className="w-full flex justify-center md:justify-end">
-        <Button className="w-fit text-secondary cursor-pointer" variant={"outline"} size={"lg"} onClick={() => setRules(state => !state)}>Rules</Button>
+        <Button
+          className="w-fit text-secondary cursor-pointer"
+          variant={"outline"}
+          size={"lg"}
+          onClick={() => setRules((state) => !state)}
+        >
+          Rules
+        </Button>
       </div>
-
-      {rules && (
-        <Rules rules={rules} setRules={setRules} />
-      )}
-
-
+      {rules && <Rules rules={rules} setRules={setRules} />}
     </div>
   );
 }
